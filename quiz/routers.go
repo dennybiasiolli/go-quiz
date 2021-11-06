c265ad487b57123171b1ca1b4adec08a6a03fdba
Denny Biasiolli <denny.biasiolli@gmail.com>
2021-11-06 21:54:01 +0100

    quiz: adding quizzes and questions update with PUT method

--- quiz/routers.go
+++ quiz/routers.go
@@ -10,7 +10,9 @@ func QuizRoutesRegister(router fiber.Router) {
 	quizzes.Get("/", QuizList)
 	quizzes.Get("/:id/", QuizDetail)
 	quizzes.Use(auth.IsAdminMiddleware()).Post("/", QuizCreate)
+	quizzes.Use(auth.IsAdminMiddleware()).Put("/:id/", QuizUpdate)
 
 	questions := router.Group("/questions")
 	questions.Use(auth.IsAdminMiddleware()).Post("/", QuestionCreate)
+	questions.Use(auth.IsAdminMiddleware()).Put("/:id/", QuestionUpdate)
 }
